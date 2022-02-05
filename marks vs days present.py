5f87c342b9a49d341fbc02e3afb588701ed33a00
Suhani31Agrawal <73304019+Suhani31Agrawal@users.noreply.github.com>
2022-02-05 09:32:23 +0530

    Update marks vs days present.py

--- marks vs days present.py
+++ marks vs days present.py
@@ -1,6 +1,8 @@
 import csv
 from statistics import correlation
 import numpy as np
+import pandas as pd
+import plotly.express as px
 
 def getDataSource(dataPath):
     marks_in_percentage=[]
@@ -20,5 +22,10 @@ def setUp():
     dataPath="Student Marks vs Days Present.csv"
     dataSource=getDataSource(dataPath)
     findCorrelation(dataSource)
+
+def graphplot():
+    df=pd.read_csv("cups of coffee vs hours of sleep.csv")
+    fig=px.bar(df,x="week",y="Coffee in ml",color="sleep in hours",title="COFFEE INTAKE AND SLEEP TAKEN")
+    fig.show()
 
 setUp()
